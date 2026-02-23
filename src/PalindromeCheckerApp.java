2c3724c570b8316f07f06e03157a518c3e1b6027
RA2411026011366 <vm0982@srmist.edu.in>
2026-02-23 10:37:06 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,9 @@
 public class PalindromeCheckerApp {
+    public static void main(String[]args){
+        System.out.println("Welcome to Palindrome Checker app");
+        System.out.println("The version of this app is 1.0.0");
+        System.out.println("Palindrome checker started successfully");
+
+
+    }
 }
